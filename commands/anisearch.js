7f4629182f008faced3d029672c906c1e6a45593
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-08-20 22:55:55 -0700

    add jikan wrapper and first anime search command

---
+++ commands/anisearch.js
@@ -0,0 +1,23 @@
+const Jikan = require('jikan-node');
+const ji = new Jikan();
+
+module.exports = {
+	name: 'anisearch',
+	description: 'Search for your anime',
+	execute(message, args) {
+		console.log(args);
+		ji.search(args[0], args[1])
+			.then(info=>{
+				const animeList = info.results.map(data => {
+					return `\n**Title**: ${data.title} was found. \n**Description**: ${data.synopsis}`;
+				});
+				animeList.length = 5;
+				message.channel.send(animeList);
+			})
+			.catch(err=>{
+				console.log(err);
+				message.channel.send('There was an error! Please try again.');
+			});
+
+	},
+};
